62a9186f5d706c6ce5ac0c480e070576f44d385f
Viktoruin <viktor15gorelov@gmail.com>
2024-07-31 11:31:09 +0300

    base

---
+++ src/main/java/com/example/Okten_Java_Springboot/OktenJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.Okten_Java_Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktenJavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OktenJavaSpringbootApplication.class, args);
+	}
+
+}
